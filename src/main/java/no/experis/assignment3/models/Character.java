70130f7cf29097a00dd2a4589e218f784b8f8e04
lucastrann <71780014+lucastrann@users.noreply.github.com>
2023-08-25 14:58:16 +0200

    setting character id to auto inc

--- src/main/java/no/experis/assignment3/models/Character.java
+++ src/main/java/no/experis/assignment3/models/Character.java
@@ -1,11 +1,12 @@
 package no.experis.assignment3.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Character {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "char_id")
     private int id;
 
     public Character() {
