bd4916c7f7afb03bf6275ed67dbcee08d0918699
lucastrann <71780014+lucastrann@users.noreply.github.com>
2023-08-25 14:19:27 +0200

    initial setup of models

--- src/main/java/no/experis/assignment3/models/Character.java
+++ src/main/java/no/experis/assignment3/models/Character.java
@@ -3,12 +3,26 @@ package no.experis.assignment3.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+import java.net.URL;
+
 @Entity
 public class Character {
     @Id
     private int id;
+    private String name;
+    private String alias;
+    private String gender;
+    private URL photo;
 
     public Character() {
+    }
+
+    public Character(int id, String name, String alias, String gender, URL photo) {
+        this.id = id;
+        this.name = name;
+        this.alias = alias;
+        this.gender = gender;
+        this.photo = photo;
     }
 
     public void setId(int id) {
@@ -17,5 +31,37 @@ public class Character {
 
     public int getId() {
         return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public URL getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(URL photo) {
+        this.photo = photo;
     }
 }
--- src/main/java/no/experis/assignment3/models/Movies.java
+++ src/main/java/no/experis/assignment3/models/Movies.java
@@ -3,10 +3,18 @@ package no.experis.assignment3.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+import java.net.URL;
+
 @Entity
 public class Movies {
     @Id
     private int id;
+    private String title;
+    private String genre;
+    private int year;
+    private String director;
+    private URL poster;
+    private URL trailer;
 
     public Movies() {
     }
@@ -17,5 +25,53 @@ public class Movies {
 
     public int getId() {
         return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public URL getPoster() {
+        return poster;
+    }
+
+    public void setPoster(URL poster) {
+        this.poster = poster;
+    }
+
+    public URL getTrailer() {
+        return trailer;
+    }
+
+    public void setTrailer(URL trailer) {
+        this.trailer = trailer;
     }
 }
