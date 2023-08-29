b8f697ca3b02e5ad6da98fb53bab05eb0d080603
lucastrann <71780014+lucastrann@users.noreply.github.com>
2023-08-29 14:26:52 +0200

    added CRUDService

---
+++ src/main/java/no/experis/assignment3/services/CRUDService.java
@@ -0,0 +1,14 @@
+package no.experis.assignment3.services;
+
+import java.util.Collection;
+
+public interface CRUDService <T, ID> {
+    // Generic CRUD
+    T findById(ID id);
+    Collection<T> findAll();
+    T add(T entity);
+    T update(T entity);
+    void deleteById(ID id);
+    void delete(T entity);
+}
+
